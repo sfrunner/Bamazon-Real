87b128cdb4eb260d3bf289b18050a688bd2b517b
Steven Fox <sfrunner1188@gmail.com>
2017-04-20 21:19:51 -0700

    Creation of bamazonSupervisor.js and update of bamazon.js

--- Bamazon-Real/bamazonSupervisor.js
+++ Bamazon-Real/bamazonSupervisor.js
@@ -1,5 +1,6 @@
 ﻿var inquirer = require("inquirer");
 var mysql = require("mysql");
+var table = require("cli-table");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -30,8 +31,27 @@ require("jsdom").env("", function (err, window) {
     ]).then(function (answers) {
 
         switch (answers.action) {
-            case "View Product Sales by Department": viewProducts(); break;
-            case "Create New Department": lowInventory(); break;
-            default: viewProducts();
+            case "View Product Sales by Department": viewDepartments(); break;
+            //case "Create New Department": lowInventory(); break;
+            default: viewDepartments();
         }
     });
+
+    function viewDepartments() {
+
+        // instantiate 
+        var Table = new table({
+            head: ["department+id","department_name","over_head_costs","product_sales","total_profit"]
+            , colWidths: [100, 100,100,100,100]
+        });
+
+        // table is an Array, so you can `push`, `unshift`, `splice` and friends 
+        Table.push(
+            ['First value', 'Second value']
+            , ['First value', 'Second value']
+        );
+
+        console.log(Table.toString());
+    }
+
+});
